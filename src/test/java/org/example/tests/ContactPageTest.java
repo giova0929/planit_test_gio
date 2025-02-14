21db7c17cd27b544a656f3d1662fd1565eb3e8e0
Giovanni <giovannycorrales89@gmail.com>
2025-02-14 15:30:29 +1030

    first commit

---
+++ src/test/java/org/example/tests/ContactPageTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class ContactPageTest {
+    @Test
+    public void sampleTest() {
+        System.out.println("TestNG está funcionando correctamente.");
+    }
+}
